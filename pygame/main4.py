89fbce60aeafb0ab99a8a8d12f983ff0fdd7a4ee
Michał Grabowski <mkg.grabowski@gmail.com>
2017-12-14 13:10:38 +0100

    Benchmarking plans.

---
+++ pygame/main4.py
@@ -0,0 +1,12 @@
+import resource
+import time
+
+"""
+Here will go benchmarking methods for ops coded for lab2 and lab3.
+
+Plan:
+1. Measure time per execution of surface processing methods to measure FPS:
+    -on regular PC
+    -on RPi
+2. Use Pycharm's built-in profiler to get specific data about performance.
+"""
