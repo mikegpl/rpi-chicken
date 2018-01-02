517f9d21b30fe073b105b39200e3f0d36a47b999
Michał Grabowski <mkg.grabowski@gmail.com>
2018-01-02 14:26:30 +0100

    .gitignore for PyCharm, rusage -> timeit for time measurements, output formatting

--- pygame-vs-opencv/main5.py
+++ pygame-vs-opencv/main5.py
@@ -3,7 +3,7 @@ import pyximport
 pyximport.install()
 
 import cv2
-import resource
+import timeit
 import numpy as np
 
 from lab2 import blur_utils as bu
@@ -12,16 +12,6 @@ from lab3 import edge_utils as eu
 from main2 import transform_by_kernel
 from main3 import canny_edge_detection
 import main4 as m4
-
-"""
-Here will go benchmarking methods for ops coded for lab2 and lab3.
-
-Plan:
-1. Measure time per execution of surface processing methods to measure FPS:
-    -on regular PC
-    -on RPi
-2. Use Pycharm's built-in profiler to get specific data about performance.
-"""
 
 IMG_PATH = "img/cat.jpg"
 MEASUREMENTS = 20
@@ -37,21 +27,18 @@ for i in range(1, 7, 2):
 def measure_execution_time(fun, times=1):
     measure_points = []
     for _ in range(0, times):
-        start = resource.getrusage(resource.RUSAGE_SELF)
+        start = timeit.default_timer()
         fun()
-        end = resource.getrusage(resource.RUSAGE_SELF)
-        system = end.ru_stime - start.ru_stime
-        user = end.ru_utime - start.ru_utime
-        measure_points.append((system, user))
+        end = timeit.default_timer()
+        measure_points.append(end - start)
 
     return measure_points
 
 
-def print_measurements(measurements):
+def print_measurements(source, measurements):
     for description in measurements:
-        print(description)
-        print("Avg {}\tstdev {}".format(np.mean(measurements[description]), np.std(measurements[description])))
-        print("###\n")
+        print("{},{},{},{}".format(source, description, np.mean(measurements[description]),
+                                   np.std(measurements[description])))
 
 
 def benchmark_lab2():
@@ -65,16 +52,16 @@ def benchmark_lab2():
     source_img = pgu.img_to_surface(IMG_PATH)
 
     for kernel_size in AVERAGES:
-        data_points["Pygame Average " + str(kernel_size)] = measure_execution_time(
+        data_points["average " + str(kernel_size)] = measure_execution_time(
             lambda: transform_by_kernel(source_img, AVERAGES[kernel_size]),
             MEASUREMENTS)
 
     for kernel_size in GAUSSIAN:
-        data_points["Pygame Gaussian " + str(kernel_size)] = measure_execution_time(
+        data_points["gaussian " + str(kernel_size)] = measure_execution_time(
             lambda: transform_by_kernel(source_img, GAUSSIAN[kernel_size]),
             MEASUREMENTS)
 
-    print_measurements(data_points)
+    print_measurements("Pygame", data_points)
 
 
 def benchmark_lab3():
@@ -86,7 +73,7 @@ def benchmark_lab3():
 
     source_img = pgu.img_to_surface(IMG_PATH)
     data_points = {
-        "Pygame To greyscale": measure_execution_time(lambda: eu.surf_to_greyscale(source_img), MEASUREMENTS)}
+        "greyscale": measure_execution_time(lambda: eu.surf_to_greyscale(source_img), MEASUREMENTS)}
 
     def canny(image, kernel=None):
         image = eu.surf_to_greyscale(image)
@@ -95,7 +82,7 @@ def benchmark_lab3():
         canny_edge_detection(image, SOBEL_SUM)
 
     def canny_without_blur():
-        tag = "Pygame Canny without blur"
+        tag = "canny without blur"
         data_points[tag] = []
         for _ in range(MEASUREMENTS):
             img = pgu.img_to_surface(IMG_PATH)
@@ -103,7 +90,7 @@ def benchmark_lab3():
 
     def canny_with_blur():
         # with 3x3 gaussian kernel
-        tag = "Pygame Canny with 3x3 gaussian blur"
+        tag = "canny with 3x3 gaussian blur"
         data_points[tag] = []
         for _ in range(MEASUREMENTS):
             img = pgu.img_to_surface(IMG_PATH)
@@ -113,7 +100,7 @@ def benchmark_lab3():
     canny_without_blur()
     canny_with_blur()
 
-    print_measurements(data_points)
+    print_measurements("Pygame", data_points)
 
 
 def benchmark_lab4():
@@ -137,42 +124,40 @@ def benchmark_lab4():
 
     # blurring - transform by kernel using filter2D
     for kernel_size in AVERAGES:
-        data_points["CV Average filter2D " + str(kernel_size)] = measure_execution_time(
+        data_points["average filter2D " + str(kernel_size)] = measure_execution_time(
             lambda: m4.transform_by_kernel(img, AVERAGES[kernel_size][0] / AVERAGES[kernel_size][1]),
             MEASUREMENTS)
 
     for kernel_size in GAUSSIAN:
-        data_points["CV Gaussian filter2D " + str(kernel_size)] = measure_execution_time(
+        data_points["gaussian filter2D " + str(kernel_size)] = measure_execution_time(
             lambda: m4.transform_by_kernel(img, GAUSSIAN[kernel_size][0] / GAUSSIAN[kernel_size][1]),
             MEASUREMENTS)
 
     # blurring using native cv2 methods
     for k in range(1, 7, 2):
-        data_points["CV Average native " + str(k)] = measure_execution_time(
+        data_points["average native " + str(k)] = measure_execution_time(
             lambda: m4.avg_blur(img, k),
             MEASUREMENTS)
-        data_points["CV Gaussian native " + str(k)] = measure_execution_time(
+        data_points["gaussian native " + str(k)] = measure_execution_time(
             lambda: m4.gaussian_blur(img, k),
             MEASUREMENTS)
 
     # img to greyscale
-    data_points["CV To greyscale"] = measure_execution_time(lambda: m4.image_to_greyscale(img), MEASUREMENTS)
+    data_points["greyscale"] = measure_execution_time(lambda: m4.image_to_greyscale(img), MEASUREMENTS)
 
     # canny without blur
-    data_points["CV Canny without blur"] = measure_execution_time(lambda: m4.canny_edge_detection(img, False),
-                                                                  MEASUREMENTS)
+    data_points["canny without blur"] = measure_execution_time(lambda: m4.canny_edge_detection(img, False),
+                                                               MEASUREMENTS)
 
     # canny with 3x3 gaussian blur
-    data_points["CV Canny with 3x3 gaussian blur"] = measure_execution_time(lambda: m4.canny_edge_detection(img, True),
-                                                                            MEASUREMENTS)
+    data_points["canny with 3x3 gaussian blur"] = measure_execution_time(lambda: m4.canny_edge_detection(img, True),
+                                                                         MEASUREMENTS)
 
-    print_measurements(data_points)
+    print_measurements("OpenCV", data_points)
 
 
 if __name__ == "__main__":
-    print("Pygame")
+    print("source, method, avg time, time std dev")
     benchmark_lab2()
     benchmark_lab3()
-    print("\n\n\n")
-    print("OpenCV")
     benchmark_lab4()
